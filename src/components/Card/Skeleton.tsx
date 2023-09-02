553bfbe3b350dcc44e1cb58ccf9f210033c40370
Dina Migunova <migunova.dina@gmail.com>
2023-09-02 22:53:06 +0200

    Change Skeleton style

--- src/components/Card/Skeleton.tsx
+++ src/components/Card/Skeleton.tsx
@@ -3,21 +3,16 @@ import ContentLoader from "react-content-loader"
 
 const Skeleton = (props) => (
     <ContentLoader
-        className="card-block" 
+        className="skeleton" 
         speed={0.5}
-        width={280}
-        height={500}
-        viewBox="0 0 280 500"
+        width={255}
+        height={571}
+        viewBox="0 0 255 571"
         backgroundColor="#e9e9e9"
         foregroundColor="#d3d3d3"
         {...props}
     >
-        <rect x="0" y="0" rx="50" ry="50" width="260" height="260" /> 
-        <rect x="63" y="102" rx="0" ry="0" width="28" height="29" /> 
-        <rect x="0" y="280" rx="10" ry="10" width="260" height="24" /> 
-        <rect x="0" y="315" rx="10" ry="10" width="260" height="88" /> 
-        <rect x="0" y="417" rx="10" ry="10" width="90" height="30" /> 
-        <rect x="128" y="417" rx="10" ry="10" width="130" height="42" />
+<rect x="-1" y="-16" rx="21" ry="21" width="255" height="571" />
     </ContentLoader>
 )
 
