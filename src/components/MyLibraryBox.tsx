2c492ddade8d2f0c390c840cf758d8b611e974d6
Dina Migunova <migunova.dina@gmail.com>
2023-09-02 19:12:08 +0200

    Create My library page

--- src/components/MyLibraryBox.tsx
+++ src/components/MyLibraryBox.tsx
@@ -3,7 +3,24 @@ import React from 'react';
 const MyLibraryBox: React.FC = () => {
     return (
         <div>
-            MyLibraryBox
+            <ul>
+                <li>
+                    My books
+                    <span>2</span>
+                </li>
+                <li>
+                    Want to read
+                    <span>2</span>
+                </li>
+                <li>
+                    My history
+                    <span>2</span>
+                </li>
+                <li>
+                    Books to send
+                    <span>2</span>
+                </li>
+            </ul>
         </div>
     )}
 
