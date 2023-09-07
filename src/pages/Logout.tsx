a321cb560ff76adbb718998795166e90fb406b52
Maryna Pogrebniak <ms.pogrebnyak@gmail.com>
2023-09-07 12:52:08 +0200

    fix logout. add styles to buttons

--- src/pages/Logout.tsx
+++ src/pages/Logout.tsx
@@ -1,32 +1,33 @@
 import axios from 'axios';
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom'; // Импортируем useNavigate
 import { setUser } from '../redux/slices/usersSlice';
 
-
-
 const Logout: React.FC = () => {
-    
     const dispatch = useDispatch();
+    const navigate = useNavigate(); // Получаем функцию navigate
 
     const handleLogout = async () => {
         try {
-            await axios.post(`/api/logout`, { withCredentials: true});
-            dispatch(setUser(null))
+            await axios.post(`/api/logout`, { withCredentials: true });
+            dispatch(setUser(null));
+
+            // Выполняем перенаправление на главную страницу
+            navigate('/');
         } catch (error) {
+            // Обработка ошибок
         }
     }
 
-
     return (
         <div className='container'>
-            Login
+          <h2 className="content__title"> Are you sure you want to logout?</h2> 
             <div>
-            <button type='button' onClick={handleLogout} className='button'>Yes</button>
-            <Link to="/" className='button'>No</Link>
-            </div>
+                <button type='button' onClick={handleLogout} className='button button__yesno'>Yes</button>
+                <Link to="/" className='button button__yesno'>No</Link></div>
         </div>
-    )}
+    )
+}
 
-    export default Logout;
+export default Logout;
