61ee5699d2eebb5ac219b2c1f7d5cff85f6123c5
ArtemShvidchenko <99479370+ArtemShvidchenko@users.noreply.github.com>
2023-09-08 02:12:29 +0200

    Update TermsAndConditions.tsx
    Update Terms

--- src/components/TermsAndConditions.tsx
+++ src/components/TermsAndConditions.tsx
@@ -10,17 +10,24 @@ const TermsAndConditions: React.FC = () => {
             <h2 className='h'>
                 Terms and Conditions
             </h2>
+            <div className='content1'>
+            <h6 className='h6'>
+                Agreement to our legal terms
+            </h6>
             <p className='text'>
-            Lacus habitasse neque, scelerisque aliquet. Nec, bibendum viverra vitae, molestie cum ut. Pharetra lectus volutpat arcu ut ultrices eu sit volutpat. Pretium egestas in massa cursus ornare. Amet, non gravida rutrum luctus 
-            Lacus habitasse neque, scelerisque aliquet. Nec, bibendum viverra vitae, molestie cum ut. Pharetra lectus volutpat arcu ut ultrices eu sit volutpat. Pretium egestas in massa cursus ornare. Amet, non gravida rutrum luctus 
-            Lacus habitasse neque, scelerisque aliquet. Nec, bibendum viverra vitae, molestie cum ut. Pharetra lectus volutpat arcu ut ultrices eu sit volutpat. Pretium egestas in massa cursus ornare. Amet, non gravida rutrum luctus 
-            Lacus habitasse neque, scelerisque aliquet. Nec, bibendum viverra vitae, molestie cum ut. Pharetra lectus volutpat arcu ut ultrices eu sit volutpat. Pretium egestas in massa cursus ornare. Amet, non gravida rutrum luctus.
-            </p>
-            <button className='agree' >
+                Lacus habitasse neque, scelerisque aliquet. Nec, bibendum viverra vitae, molestie cum ut. Pharetra lectus volutpat arcu ut ultrices eu sit volutpat. Pretium egestas in massa cursus ornare. Amet, non gravida rutrum luctus 
+                Lacus habitasse neque, scelerisque aliquet. Nec, bibendum viverra vitae, molestie cum ut. Pharetra lectus volutpat arcu ut ultrices eu sit volutpat. Pretium egestas in massa cursus ornare. </p> 
+            <p className='text1'>
+                Amet, non gravida rutrum luctus Lacus habitasse neque, scelerisque aliquet. Nec, bibendum viverra vitae, molestie cum ut. Pharetra lectus volutpat arcu ut ultrices eu sit volutpat. Pretium egestas in massa cursus ornare.</p>
+            <p className='text2'> 
+                Amet, non gravida rutrum luctus Lacus habitasse neque, scelerisque aliquet. Nec, bibendum viverra vitae, molestie cum ut. Pharetra lectus volutpat arcu ut ultrices eu sit volutpat. Pretium egestas in massa cursus ornare. Amet, non gravida rutrum luctus.</p>
+        
+            <button className='button' >
                 <Link to="/"> 
-                    I agree 
+                    <span> I agree </span>
                 </Link>
             </button>
+            </div>   
         </div>
     )
     
